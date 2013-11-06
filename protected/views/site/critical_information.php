14d368ebb6b7d24d03f4ddaa1fae58975e76b308
danesh <danesh@danesh-PC>
2013-11-06 06:21:02 +0530

    Admin control development

--- protected/views/site/critical_information.php
+++ protected/views/site/critical_information.php
@@ -2,26 +2,28 @@
     #sidebar{ display: none; }
 </style>
 <div class="column" style="background: white; padding: 10px 20px 10px 20px; width: 1134px; margin-left: -1px; margin: 0px; font-size: 16px; color: black">
-    <p>Further to your inquiry with YMB.</p>
-    <p>We have tried to include as much basic information as possible but, without exception, clients have specific questions they would like answered quickly.</p>
-    <p>I will contact you at the telephone number you have provided within one business day so it would be helpful to write down your questions in advance so we can cover the answers in just a few minutes.</p>
-    <p style="text-align: center"><img src="images/rob_in_email.jpg"/></p>
-    <p>As a supplement to our discussion I strongly recommend you read the 456 Page best seller.</p>
-    <p style="color: red; font-weight: bold">Retire to Mexico – Supercharge your Dollar – Become a Middleclass Millionaire… <br/><font style="color: black; font-weight: normal">is the missing link between your research on the internet and the reality at ground zero.</font></p>
-    <p>If you plan on investing more than $ 30 in Mexico, this should be the first $30.</p>
-    <p><a href="http://amzn.to/iiRbyt">http://amzn.to/iiRbyt</a></p>
-    <p>Discover How To:</p>
-    <p style="color: red; font-weight: bold">Supercharge your dollar…<br/><font style="color: black; font-weight: normal">Live like a king on $20K a year, including health insurance – guaranteed!!</font></p>
-    <p style="color: red; font-weight: bold">Prospering with a business based in Mexico………<br/><font style="color: black; font-weight: normal">Navigating the secret trap doors of Mexican commerce to find your international gold !!</font></p>
-    <p style="color: red; font-weight: bold">Escape the North American "rat race" and "tax pace" for good…<br/><font style="color: black; font-weight: normal">Enjoy low taxes, fewer laws, near perfect weather and a hassle free lifestyle!!</font></p>
-    <p style="color: red; font-weight: bold">Finding great Mexican real estate…………<br/><font style="color: black; font-weight: normal">The “A to Z” on formulas, what to avoid, how to minimize risk and maximize profits, all from industry insiders!!</font></p>
-    <p style="color: red; font-weight: bold"><u>20 things you need to do</u> "before you leave" your country of origin…<br/><font style="color: black; font-weight: normal">Set up your infrastructure correctly the first time so you have choices later!!!</font></p>
-    <p>Author Bio: Robert Harker is an established entrepreneur, realtor, writer and expat who has successfully navigated the rocky road to prosperity and a far better life in Mexico.</p>
-    <p style="font-size: 22px; font-weight: bold; text-align: center">You can too!!!!!</p>
-    <p style="text-align: center"><img src="images/book_in_email.jpg"/></p>
-    <p style="color: red; font-weight: bold">Your accommodations are free !!!!!! When you buy and renovate with YMB.</p>
-    <p style="text-align: center"><img src="images/house_in_email.jpg"/></p>
-    <p>Check out the link <a href="http://yucatanmexicobeachrentals.com">http://yucatanmexicobeachrentals.com/</a></p>
-    <p>Let’s talk soon and start moving toward the life you have always wanted.</p>
-    <p><b>Robert Harker</b><br/>Skype: yucatanrob, Virtual number: 702-997-3047 local cell: 52-1-999-129-7321 Off: 52-969-934-1080</p>
+    <div style="padding: 0 180px 0 180px; text-align: justify">
+        <p>Further to your inquiry with YMB.</p>
+        <p>We have tried to include as much basic information as possible but, without exception, clients have specific questions they would like answered quickly.</p>
+        <p>I will contact you at the telephone number you have provided within one business day so it would be helpful to write down your questions in advance so we can cover the answers in just a few minutes.</p>
+        <p style="text-align: center"><img src="images/rob_in_email.jpg"/></p>
+        <p>As a supplement to our discussion I strongly recommend you read the 456 Page best seller.</p>
+        <p style="color: red; font-weight: bold">Retire to Mexico – Supercharge your Dollar – Become a Middleclass Millionaire… <br/><font style="color: black; font-weight: normal">is the missing link between your research on the internet and the reality at ground zero.</font></p>
+        <p>If you plan on investing more than $ 30 in Mexico, this should be the first $30.</p>
+        <p><a href="http://amzn.to/iiRbyt">http://amzn.to/iiRbyt</a></p>
+        <p>Discover How To:</p>
+        <p style="color: red; font-weight: bold">Supercharge your dollar…<br/><font style="color: black; font-weight: normal">Live like a king on $20K a year, including health insurance – guaranteed!!</font></p>
+        <p style="color: red; font-weight: bold">Prospering with a business based in Mexico………<br/><font style="color: black; font-weight: normal">Navigating the secret trap doors of Mexican commerce to find your international gold !!</font></p>
+        <p style="color: red; font-weight: bold">Escape the North American "rat race" and "tax pace" for good…<br/><font style="color: black; font-weight: normal">Enjoy low taxes, fewer laws, near perfect weather and a hassle free lifestyle!!</font></p>
+        <p style="color: red; font-weight: bold">Finding great Mexican real estate…………<br/><font style="color: black; font-weight: normal">The “A to Z” on formulas, what to avoid, how to minimize risk and maximize profits, all from industry insiders!!</font></p>
+        <p style="color: red; font-weight: bold"><u>20 things you need to do</u> "before you leave" your country of origin…<br/><font style="color: black; font-weight: normal">Set up your infrastructure correctly the first time so you have choices later!!!</font></p>
+        <p>Author Bio: Robert Harker is an established entrepreneur, realtor, writer and expat who has successfully navigated the rocky road to prosperity and a far better life in Mexico.</p>
+        <p style="font-size: 22px; font-weight: bold; text-align: center">You can too!!!!!</p>
+        <p style="text-align: center"><img src="images/book_in_email.jpg"/></p>
+        <p style="color: red; font-weight: bold; text-align: center">Your accommodations are free !!!!!! When you buy and renovate with YMB.</p>
+        <p style="text-align: center"><img src="images/house_in_email.jpg"/></p>
+        <p style="text-align: center">Check out the link <a href="http://yucatanmexicobeachrentals.com">http://yucatanmexicobeachrentals.com/</a></p>
+        <p style="text-align: center">Let’s talk soon and start moving toward the life you have always wanted.</p>
+        <p style="text-align: center"><b>Robert Harker</b><br/>Skype: yucatanrob, Virtual number: 702-997-3047 <br/> local cell: 52-1-999-129-7321 Off: 52-969-934-1080</p>
+    </div>
 </div>        
